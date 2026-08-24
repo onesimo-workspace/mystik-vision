c09b92627678a994f3fd4589708fc46d5136b19b
Onesimo Dayeli <126966352+onesimo-workspace@users.noreply.github.com>
2026-08-24 10:32:35 +0200

    Separate Azure provider from analysis engine

--- Program.cs
+++ Program.cs
@@ -1,25 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddMemoryCache();
 builder.Services.AddScoped<app_dev_assignment.Services.IBlobService, app_dev_assignment.Services.BlobService>();
 builder.Services.AddSingleton<app_dev_assignment.Services.IHistoryService, app_dev_assignment.Services.HistoryService>();
-builder.Services.AddHttpClient<app_dev_assignment.Services.IVisionService, app_dev_assignment.Services.VisionService>();
+builder.Services.AddHttpClient<app_dev_assignment.Services.IVisionAnalysisProvider, app_dev_assignment.Services.AzureVisionAnalysisProvider>();
+builder.Services.AddScoped<app_dev_assignment.Services.IVisionService, app_dev_assignment.Services.VisionService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-}
+if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Home/Error");
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
